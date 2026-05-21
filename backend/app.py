89453c499f790d03621ad116f6cfeabbc5fff138
inamfr <inamkhanphysics369@gmail.com>
2026-05-21 11:02:13 +0000

    Add detailed error logging

--- backend/app.py
+++ backend/app.py
@@ -96,6 +96,8 @@ def issue_certificate():
         }), 200
 
     except Exception as e:
+        import traceback
+        print(traceback.format_exc())
         return jsonify({"error": str(e)}), 500
 
     finally:
